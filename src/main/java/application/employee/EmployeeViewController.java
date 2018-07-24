39fb46e450354c700612047cd54581d55fb0d0d0
Mathieu Themelin <mat.themelin@hotmail.fr>
2018-07-24 13:52:53 +0200

    modif nom fichier xml CamelCase

--- src/main/java/application/employee/EmployeeViewController.java
+++ src/main/java/application/employee/EmployeeViewController.java
@@ -205,7 +205,7 @@ public class EmployeeViewController extends MainViewController {
 		dialogVbox.getChildren().add(paneDelete);
 		scene = new Scene(dialogVbox, 400, 600);
 		detail.setScene(scene);
-		FXMLLoader loader = new FXMLLoader(getClass().getResource("employeedelete.fxml"));
+		FXMLLoader loader = new FXMLLoader(getClass().getResource("EmployeeDelete.fxml"));
 		try {
 			scene.setRoot((Parent) loader.load());
 		} catch (IOException e) {
@@ -227,7 +227,7 @@ public class EmployeeViewController extends MainViewController {
 		dialogVbox.getChildren().add(new Text("Nouveau Personnel"));
 		scene = new Scene(dialogVbox, 400, 600);
 		create.setScene(scene);
-		FXMLLoader loader = new FXMLLoader(getClass().getResource("employeecreate.fxml"));
+		FXMLLoader loader = new FXMLLoader(getClass().getResource("EmployeeCreate.fxml"));
 		try {
 			scene.setRoot((Parent) loader.load());
 		} catch (IOException e) {
